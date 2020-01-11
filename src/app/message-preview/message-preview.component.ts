1c6b0a680c74f14ea265b50701cf56cf18047d7c
MarlyssaB <marlyssa.boucher@gmail.com>
2020-01-11 15:54:40 -0600

    final version

---
+++ src/app/message-preview/message-preview.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-message-preview',
+  templateUrl: './message-preview.component.html',
+  styleUrls: ['./message-preview.component.css']
+})
+export class MessagePreviewComponent implements OnInit {
+  @Input() message = {}
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
